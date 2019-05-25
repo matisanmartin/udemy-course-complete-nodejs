ba456a2f4a24057b6a6560fa74214750fbdffd2d
Matias San Martin <msanmartinacosta@gmail.com>
2019-05-25 20:34:06 -0300

    Adds yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,43 @@
-const getNotes = require('./notes.js')
 const chalk = require('chalk')
-
-const notes = getNotes()
-console.log(notes)
-
-console.log(chalk.red.bold.inverse('Error'))
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
 
+//Customize yargs version
+yargs.version('1.1.0')
+
+// add, remove, read, list
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: function() {
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists al the notes',
+    handler: function() {
+        console.log('Listing al the notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    handler: function() {
+        console.log('Reading note')
+    }
+})
+
+console.log(yargs.argv)
 
