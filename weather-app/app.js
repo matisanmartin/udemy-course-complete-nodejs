cfa34d4dbf0d86fbbbbddeedf1b8e33d83f97e48
Matias San Martin <msanmartinacosta@gmail.com>
2019-05-26 22:02:15 -0300

    Adds weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,16 @@
-console.log('Starting')
+const request = require('request')
 
+const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Buenos%20Aires?access_token=pk.eyJ1IjoibXNhbm1hcnRpbmFjb3N0YSIsImEiOiJjanc1bm80c24xMWgyNDBvd2k2Z2N3NTZrIn0.Pot2zT3o-XP0626sJdr_Aw'
+const urlDarkSky = 'https://api.darksky.net/forecast/8971633f2bbdf443903eb6d5ffe6a5c0/37.8267,-122.4233?units=si'
 
-console.log('Stopping')
+request({url: urlMapbox, json: true}, (error, response) => {
+    const center = response.body.features[0].center
+    const latitude = center[1]
+    const longitude = center[0]
+})
+
+request({ url: urlDarkSky, json: true }, (error, response) => {
+    const currently = response.body.currently
+    const summary = response.body.daily.data[0].summary
+    console.log(summary + ' It is currently ' + currently.temperature + ' degrees. There is a ' + currently.precipProbability + '% chance of rain')
+})
