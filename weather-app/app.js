37956ac0b274bf60e6a61459a61fff527acbf930
Matias San Martin <msanmartinacosta@gmail.com>
2019-05-28 16:23:48 -0300

    End of section 6

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,14 +1,44 @@
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const yargs = require('yargs')
 
-const urlMapbox = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Buenos%20Aires.json?access_token=pk.eyJ1IjoibXNhbm1hcnRpbmFjb3N0YSIsImEiOiJjanc1bm80c24xMWgyNDBvd2k2Z2N3NTZrIn0.Pot2zT3o-XP0626sJdr_Aw'
-const urlDarkSky = 'https://api.darksky.net/forecast/8971633f2bbdf443903eb6d5ffe6a5c0/37.8267,-122.4233?units=si'
+yargs.version('1.1.0')
 
-request({ url: urlDarkSky, json: true }, (error, response) => {
-    if(error) {
-        console.log("unable to connect to weather service")
-    } else {
-        const currently = response.body.currently
-        const summary = response.body.daily.data[0].summary
-        console.log(summary + ' It is currently ' + currently.temperature + ' degrees. There is a ' + currently.precipProbability + '% chance of rain')
-    } 
+yargs.command({
+    command: 'find',
+    describe: 'find a city',
+    builder: {
+        city: {
+            describe: 'city name',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler(argv) {
+        geocode(argv.city, (error, {latitude, longitude, location}) => {
+            if(error) {
+                return console.log(error)
+            }
+        
+            forecast(latitude, longitude, (error, forecastData) => {
+                if(error) {
+                    return console.log(error)
+                }
+        
+                console.log(location)
+                console.log(forecastData)
+            })
+        })
+    }
 })
+
+yargs.parse()
+
+
+
+
+
+
+
+
+
